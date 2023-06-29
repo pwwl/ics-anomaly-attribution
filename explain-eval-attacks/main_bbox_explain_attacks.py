6442d41f97bcd9fa40bcfdf6a8363320ec4c454a
sdai-andrew <sdai2@andrew.cmu.edu>
2023-06-29 16:44:22 -0400

    Minor explain attacks script fixes

--- explain-eval-attacks/main_bbox_explain_attacks.py
+++ explain-eval-attacks/main_bbox_explain_attacks.py
@@ -1,3 +1,20 @@
+"""
+
+   Copyright 2023 Lujo Bauer, Clement Fung
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+"""
 
 import lime
 import networkx as nx
@@ -163,7 +180,7 @@ if __name__ == "__main__":
 		expl = None
 
 	# Practical detection
-	detection_points = pickle.load(open('ccs-storage/detection-points.pkl', 'rb'))
+	detection_points = pickle.load(open('meta-storage/detection-points.pkl', 'rb'))
 	model_detection_points = detection_points[lookup_name]
 	explain_detect(event_detector, lookup_name, attacks, Xtest,
 			model_detection_points,

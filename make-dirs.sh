0f4afebf285f07c35dae2fc473c7b9399093630c
sdai-andrew <sdai2@andrew.cmu.edu>
2023-07-02 15:59:04 -0400

    Added script to make needed directories

---
+++ make-dirs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+mkdir meta-storage
+mkdir meta-storage/model-detection-ranks
+mkdir meta-storage/model-mses
+mkdir explanations-dir
+mkdir explanations-dir/explain23-detect-pkl
+mkdir explanations-dir/explain23-pkl
